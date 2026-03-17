513f3987d8282c01c3f56e80f94570e3338546f8
Daniel Shanahan <dshanahan1@students.ccu.edu>
2026-03-17 19:11:52 -0400

    fix: apply solution for issue #14

--- tests/lib/assert.sh
+++ tests/lib/assert.sh
@@ -41,7 +41,7 @@ assert_container_healthy() {
   local timeout=60
   local start_time=$(date +%s)
   while true; do
-    if docker ps --filter "name=$name" --filter "health=healthy" --format '{{.Names}}' | grep -q "^$name$"; then
+    if docker inspect --format '{{.State.Health.Status}}' "$name" 2>/dev/null | grep -q "healthy"; then
       return
     fi
     if (( $(date +%s) - start_time >= timeout )); then
