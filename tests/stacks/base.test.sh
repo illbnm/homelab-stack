070ce7ebeb9b87b6c6e041e457c410bcf7d9471a
Daniel Shanahan <dshanahan1@students.ccu.edu>
2026-03-17 11:59:27 -0400

    fix: apply solution for issue #14

--- tests/stacks/base.test.sh
+++ tests/stacks/base.test.sh
@@ -5,12 +5,11 @@ source ../lib/assert.sh
 test_traefik_running() {
   assert_container_running "traefik"
   assert_container_healthy "traefik"
-  assert_http_200 "http://localhost:8080/api/version"
 }
 
 test_portainer_running() {
   assert_container_running "portainer"
-  assert_http_200 "http://localhost:9000/api/status"
+  assert_http_200 "http://localhost:9000"
 }
 
 test_watchtower_running() {
