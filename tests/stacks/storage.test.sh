2c1bad8619aef70b407ea011a6621e8a2a259925
Daniel Shanahan <dshanahan1@students.ccu.edu>
2026-03-17 04:55:05 -0400

    fix: apply solution for issue #14

---
+++ tests/stacks/storage.test.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+source ../lib/assert.sh
+
+test_nextcloud_running() {
+  assert_container_running "nextcloud"
+  local result=$(curl -s "http://localhost:8080/status.php")
+  assert_http_response "$result" '"installed":true'
+}
+
+test_minio_running() {
+  assert_container_running "minio"
+}
