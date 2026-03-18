33982ca0b2ada565430c730b248d5e4dd44e6c67
Daniel Shanahan <dshanahan1@students.ccu.edu>
2026-03-18 09:56:36 -0400

    fix: apply solution for issue #14

--- tests/stacks/network.test.sh
+++ tests/stacks/network.test.sh
@@ -4,7 +4,3 @@ test_adguard_running() {
   assert_container_running "adguard"
   assert_http_200 "http://localhost:3000/control/status"
 }
-
-test_wireguard_easy_running() {
-  assert_container_running "wireguard-easy"
-}
