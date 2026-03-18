ad8c1027087e51abb2901306e66f08ac4adae71c
Daniel Shanahan <dshanahan1@students.ccu.edu>
2026-03-18 10:05:51 -0400

    fix: apply solution for issue #14

--- tests/stacks/monitoring.test.sh
+++ tests/stacks/monitoring.test.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
-test_prometheus_running() {
-  assert_container_running "prometheus"
-  assert_http_200 "http://localhost:9090/-/healthy"
-}
-
 test_grafana_running() {
   assert_container_running "grafana"
   assert_http_200 "http://localhost:3000/api/health"
+}
+
+test_prometheus_running() {
+  assert_container_running "prometheus"
+  assert_http_200 "http://localhost:9090/-/healthy"
 }
 
 test_loki_running() {
