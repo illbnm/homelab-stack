730ff1fc853ca0e47d2d0728343897b67c164495
Daniel Shanahan <dshanahan1@students.ccu.edu>
2026-03-19 22:40:49 -0400

    fix: apply solution for issue #14

--- tests/stacks/productivity.test.sh
+++ tests/stacks/productivity.test.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+source ../lib/assert.sh
+
 test_gitea_running() {
   assert_container_running "gitea"
   assert_http_200 "http://localhost:3001/api/v1/version"
@@ -7,5 +9,4 @@ test_gitea_running() {
 
 test_vaultwarden_running() {
   assert_container_running "vaultwarden"
-  assert_http_200 "http://localhost:8081"
 }
