a42822c13211286cb2a5147762e42e98079535c4
Daniel Shanahan <dshanahan1@students.ccu.edu>
2026-03-20 12:56:11 -0400

    fix: apply solution for issue #14

--- tests/stacks/sso.test.sh
+++ tests/stacks/sso.test.sh
@@ -7,6 +7,6 @@ test_authentik_running() {
   assert_http_200 "http://localhost:9001/api/v3/core/users/?page_size=1"
 }
 
-test_authentik_health() {
+test_authentik_healthy() {
   assert_container_healthy "authentik"
 }
