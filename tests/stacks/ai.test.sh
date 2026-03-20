fbcd3c8cd9dfb270cf85f7004e7bf959fba4a3e8
Daniel Shanahan <dshanahan1@students.ccu.edu>
2026-03-19 22:04:07 -0400

    fix: apply solution for issue #14

--- tests/stacks/ai.test.sh
+++ tests/stacks/ai.test.sh
@@ -4,3 +4,7 @@ test_ollama_running() {
   assert_container_running "ollama"
   assert_http_200 "http://localhost:11434/api/version"
 }
+
+test_open_webui_running() {
+  assert_container_running "open-webui"
+}
