56a7b1cbb91753df071a79a6844b772b8a4dd2e3
Daniel Shanahan <dshanahan1@students.ccu.edu>
2026-03-20 12:56:10 -0400

    fix: apply solution for issue #14

--- tests/stacks/monitoring.test.sh
+++ tests/stacks/monitoring.test.sh
@@ -12,6 +12,7 @@ test_prometheus_running() {
   assert_http_200 "http://localhost:9090/-/healthy"
 }
 
-test_loki_running() {
-  assert_container_running "loki"
+test_prometheus_scrape_cadvisor() {
+  local result=$(curl -s "http://localhost:9090/api/v1/query?query=up{job='cadvisor'}")
+  assert_json_value "$result" ".data.result[0].value[1]" "1"
 }
