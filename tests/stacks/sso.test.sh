2e9ce3236c3412741a7b2beee9e1b5dc7fdce745
Daniel Shanahan <dshanahan1@students.ccu.edu>
2026-03-20 08:55:32 -0400

    fix: apply solution for issue #14

--- tests/stacks/sso.test.sh
+++ tests/stacks/sso.test.sh
@@ -7,6 +7,6 @@ test_authentik_running() {
   assert_http_200 "http://localhost:9001/api/v3/core/users/?page_size=1"
 }
 
-test_authentik_healthy() {
-  assert_container_healthy "authentik"
+test_postgresql_running() {
+  assert_container_running "authentik-postgresql"
 }
