d68f5df69c504019dea1d9447c4725902308b408
Daniel Shanahan <dshanahan1@students.ccu.edu>
2026-03-21 09:13:11 -0400

    fix: apply solution for issue #14

--- tests/stacks/sso.test.sh
+++ tests/stacks/sso.test.sh
@@ -2,7 +2,7 @@
 
 test_authentik_running() {
   assert_container_running "authentik"
-  assert_http_200 "http://localhost:9001/api/v3/core/users/?page_size=1"
+  assert_http_200 "http://localhost:9000/api/v3/core/users/?page_size=1"
 }
 
 test_authentik_grafana_connection() {
