4fcdc9d2522d20c5f2372b19bc1fd6066110d4bf
Daniel Shanahan <dshanahan1@students.ccu.edu>
2026-03-21 09:28:03 -0400

    fix: apply solution for issue #14

--- tests/stacks/sso.test.sh
+++ tests/stacks/sso.test.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
 
+source ../lib/assert.sh
+
 test_authentik_running() {
   assert_container_running "authentik"
-  assert_http_200 "http://localhost:9000/api/v3/core/users/?page_size=1"
+  assert_http_200 "http://localhost:9001/api/v3/core/users/?page_size=1"
 }
 
-test_authentik_grafana_connection() {
-  local result=$(curl -s -u admin:${GF_ADMIN_PASSWORD} "http://localhost:3000/api/datasources/name/Prometheus")
-  assert_json_key_exists "$result" ".url"
+test_authentik_healthy() {
+  assert_container_healthy "authentik"
 }
